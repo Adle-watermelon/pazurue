3ae7bee5f42569d5f7f17b3f0c8a3c1e9f591b05
Adle-watermelon <106425536+Adle-watermelon@users.noreply.github.com>
2025-12-22 03:21:34 +0900

    修正2

--- services/db.js
+++ services/db.js
@@ -1,11 +1,9 @@
-import pkg from "pg";
-import 'dotenv/config';
-const { Pool } = pkg;
+import pg from "pg";
+const { Pool } = pg;
 
 export const pool = new Pool({
-  host: process.env.HOST,
-  port: 5432,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 });
